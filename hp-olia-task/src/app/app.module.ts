c8191f35529648b0830240c559d43a4e48556a4f
OliaSednev <oliakas999@gmail.com>
2018-05-16 21:17:18 +0300

    hp-task

---
+++ hp-olia-task/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MenuBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
